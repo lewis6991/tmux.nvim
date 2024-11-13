2d732946fa75e920d13d09469bffd78537e9dddd
Lewis Russell <lewis6991@gmail.com>
2024-11-13 11:33:55 +0000

    fold in more code

--- lua/tmux/tmux.lua
+++ lua/tmux/tmux.lua
@@ -1,3 +1,6 @@
+local config = require('tmux.config')
+local options = config.options
+
 local tmux_directions = {
   h = 'L',
   j = 'D',
@@ -5,98 +8,231 @@ local tmux_directions = {
   l = 'R',
 }
 
-local function get_tmux()
-  return vim.env.TMUX
-end
+local opposite_directions = {
+  h = 'l',
+  j = 'k',
+  k = 'j',
+  l = 'h',
+}
 
-local function get_tmux_pane()
-  return vim.env.TMUX_PANE
-end
-
-local function get_socket()
-  return vim.split(get_tmux(), ',')[1]
-end
+local is_tmux = vim.env.TMUX ~= nil
 
 --- @param arg string[]
 --- @return string
 local function execute(arg)
-  local command = vim.list_extend({ 'tmux', '-S', get_socket() }, arg)
+  local socket = vim.split(vim.env.TMUX, ',')[1]
+  local command = vim.list_extend({ 'tmux', '-S', socket }, arg)
   return vim.fn.system(command)
-end
-
---- @param arg string
---- @param pre? string
---- @return string
-local function execute2(arg, pre)
-  local command = string.format('%s tmux -S %s %s', pre or '', get_socket(), arg)
-  return vim.fn.system(command)
-end
-
-local M = {
-  is_tmux = false,
-}
-
-function M.setup()
-  M.is_tmux = get_tmux() ~= nil
-  return M.is_tmux
 end
 
 --- @param direction 'h'|'j'|'k'|'l'
-function M.change_pane(direction)
-  execute({ 'select-pane', '-t', get_tmux_pane(), '-' .. tmux_directions[direction] })
-end
-
---- @param name string
---- @return string
-function M.get_buffer(name)
-  return execute({ 'show-buffer', '-b', name })
-end
-
-function M.get_buffer_names()
-  local buffers = execute({ 'list-buffers', '-F', '#{buffer_name}' })
-
-  local result = {} --- @type string[]
-  for line in buffers:gmatch('([^\n]+)\n?') do
-    result[#result + 1] = line
-  end
-
-  return result
+local function change_pane(direction)
+  execute({ 'select-pane', '-t', vim.env.TMUX_PANE, '-' .. tmux_directions[direction] })
 end
 
 --- @return integer
-function M.get_current_pane_id()
-  return tonumber(get_tmux_pane():sub(2)) --[[@as integer]]
+local function get_current_pane_id()
+  return tonumber(vim.env.TMUX_PANE:sub(2)) --[[@as integer]]
 end
 
 --- @return string
-function M.get_window_layout()
+local function get_window_layout()
   return execute({ 'display-message', '-p', '#{window_layout}' })
 end
 
 --- @return boolean
-function M.is_zoomed()
+local function is_zoomed()
   return execute({ 'display-message', '-p', '#{window_zoomed_flag}' }):find('1') ~= nil
 end
 
 --- @param direction 'h'|'j'|'k'|'l'
 --- @param step integer
-function M.resize(direction, step)
-  execute({ 'resize-pane', '-t', get_tmux_pane(), '-' .. tmux_directions[direction], tostring(step) })
+local function resize(direction, step)
+  execute({ 'resize-pane', '-t', vim.env.TMUX_PANE, '-' .. tmux_directions[direction], tostring(step) })
 end
 
---- @param content string
---- @param sync_clipboard boolean
-function M.set_buffer(content, sync_clipboard)
-  content = content:gsub('\\', '\\\\')
-  content = content:gsub('"', '\\"')
-  content = content:gsub('`', '\\`')
-  content = content:gsub('%$', '\\$')
+--- @class tmux.layout
+--- @field width integer
+--- @field height integer
+--- @field panes table<integer,tmux.pane>
 
-  if sync_clipboard then
-    execute2('load-buffer -w -', string.format('printf "%%s" "%s" | ', content))
-  else
-    execute2('load-buffer -', string.format('printf "%%s" "%s" | ', content))
+--- @class tmux.pane
+--- @field x integer
+--- @field y integer
+--- @field width integer
+--- @field height integer
+
+--- @param x string
+--- @return integer
+local function tointeger(x)
+  return assert(tonumber(x) --[[@as integer]])
+end
+
+--- @param display string
+--- @return tmux.layout?
+local function parse(display)
+  if display == '' or not display then
+    return
   end
+
+  local panes = {} --- @type table<integer,tmux.pane>
+
+  --- @diagnostic disable-next-line:no-unknown
+  for width, height, x, y, id in display:gmatch('(%d+)x(%d+),(%d+),(%d+),(%d+)') do
+    id = tointeger(id)
+    panes[id] = {
+      x = tointeger(x),
+      y = tointeger(y),
+      width = tointeger(width),
+      height = tointeger(height),
+    }
+  end
+
+  if next(panes) == nil then
+    return
+  end
+
+  local width, height = display:match('^%w+,(%d+)x(%d+)')
+
+  if not width and not height then
+    return
+  end
+
+  return {
+    width = tointeger(width),
+    height = tointeger(height),
+    panes = panes,
+  }
+end
+
+--- @param direction 'h'|'j'|'k'|'l'
+--- @return boolean?
+local function is_border(direction)
+  local display = get_window_layout()
+  local id = get_current_pane_id()
+
+  local layout = parse(display)
+  if not layout then
+    return
+  end
+
+  local pane = layout.panes[id]
+  if not pane then
+    return
+  end
+
+  if direction == 'h' then
+    return pane.x == 0
+  elseif direction == 'j' then
+    return pane.y + pane.height == layout.height
+  elseif direction == 'k' then
+    return pane.y == 0
+  elseif direction == 'l' then
+    return pane.x + pane.width == layout.width
+  end
+  return false
+end
+
+--- @param direction 'h'|'j'|'k'|'l'
+--- @return boolean
+local function has_tmux_target(direction)
+  if not is_tmux then
+    return false
+  end
+  if is_zoomed() and options.navigation.persist_zoom then
+    return false
+  end
+  if not is_border(direction) then
+    return true
+  end
+  return options.navigation.cycle_navigation
+    and not is_border(opposite_directions[direction])
+end
+
+local function is_nvim_border(border)
+  return vim.fn.winnr() == vim.fn.winnr('1' .. border)
+end
+
+--- @param direction 'h'|'j'|'k'|'l'
+local function nav_to(direction)
+  if not is_nvim_border(direction) then
+    vim.cmd('1wincmd ' .. direction)
+  elseif has_tmux_target(direction) then
+    change_pane(direction)
+  elseif options.navigation.cycle_navigation then
+    vim.cmd('999wincmd ' .. opposite_directions[direction])
+  end
+end
+
+local function is_only_window()
+  return vim.fn.winnr('1h') == vim.fn.winnr('1l')
+    and vim.fn.winnr('1j') == vim.fn.winnr('1k')
+end
+
+local function is_tmux_target(border)
+  return is_tmux and not is_border(border) or is_only_window()
+end
+
+local M = {}
+
+function M.resize_left()
+  local step = options.resize.resize_step_x
+  if not is_nvim_border('l') then
+    vim.cmd('vertical resize +' .. step)
+  elseif is_tmux_target('l') then
+    resize('h', step)
+  else
+    vim.cmd('vertical resize -' .. step)
+  end
+end
+
+function M.resize_bottom()
+  local step = options.resize.resize_step_y
+  if not is_nvim_border('j') then
+    vim.cmd.resize('+' .. step)
+  elseif is_tmux_target('j') then
+    resize('j', step)
+  elseif vim.fn.winnr() ~= vim.fn.winnr('1k') then
+    vim.cmd.resize('-' .. step)
+  end
+end
+
+function M.resize_top()
+  local step = options.resize.resize_step_y
+  if not is_nvim_border('j') then
+    vim.cmd.resize('+' .. step)
+  elseif is_tmux_target('j') then
+    resize('k', step)
+  else
+    vim.cmd.resize('-' .. step)
+  end
+end
+
+function M.resize_right()
+  local step = options.resize.resize_step_x
+  if not is_nvim_border('l') then
+    vim.cmd('vertical resize +' .. step)
+  elseif is_tmux_target('l') then
+    resize('l', step)
+  else
+    vim.cmd('vertical resize -' .. step)
+  end
+end
+
+function M.move_left()
+  nav_to('h')
+end
+
+function M.move_bottom()
+  nav_to('j')
+end
+
+function M.move_top()
+  nav_to('k')
+end
+
+function M.move_right()
+  nav_to('l')
 end
 
 return M
